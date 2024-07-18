171ecbecf7c4c766d843462b255f032679171374
Gagandeep <gagandeep19204@gamil.com>
2024-07-18 11:41:54 +0530

    logics

---
+++ logic.js
@@ -0,0 +1,87 @@
+
+/* -------------DUPLICATES----------------*/
+
+// let carray=[1,4,2,8,4,2,8,8];
+// let dcarray=[];
+// let comary=[];
+// for(i=0; i<=carray.length; i++)
+// {
+//     dcarray=carray[i]>1
+//     console.log(carray);
+// }
+
+
+
+
+
+
+
+/*----------fibonacci series-------------- */
+// let n=prompt();
+// function fibnac(n) {
+//     let fibSeries = [];
+//     let a = 0, b = 1, c;
+//     for (i = 1; i <= n; i++) {
+//       fibSeries.push(a);
+//       c = a + b;
+//       a = b;
+//       b = c;
+//     }
+//     return fibSeries;
+//   }
+
+// document.write(fibnac(n));
+
+// function factorial(n) {
+//     let result = 1;
+//     for (i = 1; i <= n; i++) {
+//         result = result * i
+//     }
+//     return result;
+// }
+// console.log(factorial(4));
+
+
+
+
+// let ubadkhabad=[1,4,2,8,4,2,8,8];
+// console.log(ubadkhabad.sort());
+
+
+// let n="";
+// function primenum(n) {
+//     if (n <= 1) {
+//         return false;
+//     }
+//     for (let i = 2; i < n; i++) {
+//         if (n %i == 0){
+//             return false;}
+//         }
+//     return true;
+    
+// }
+// for(let n=1;n<=100;n++){
+//         if(primenum(n)){
+//             console.log(n);
+//         }
+//     }
+
+//     let array=[n.split()]
+//     console.log(array.from([n]))
+
+
+
+
+
+
+
+
+    function table(p){
+
+        for(let i=1;i<=10;i++){
+            
+            console.log(p*i)
+
+        } 
+    }
+    table(20)
