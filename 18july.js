708186795e572ab40cf59220391299de6aa44736
Gagandeep <gagandeep19204@gamil.com>
2024-07-19 15:29:32 +0530

    tunix

--- 18july.js
+++ 18july.js
@@ -1,158 +1,187 @@
-/*---------- Table ----------*/
+// /*---------- Table ----------*/
 
 
-function table(p) {
+// function table(p) {
 
-    for (let i = 1; i <= 10; i++) {
+//     for (let i = 1; i <= 10; i++) {
 
-        console.log(p * i)
+//         console.log(p * i)
 
-    }
-}
-table(20)
-
-
-/*------if,if else,elseif------- */
+//     }
+// }
+// table(20)
 
 
-
-let num = 18;                            //giving a value to num 
-if (num <= 15) {                             //checking if condition
-    console.log("Fail")                              //if condition satisfy print statement
-}
-else if (num > 15 && num < 20)                      //checking else if condition    
-{
-    console.log("passed with grace");           //if condition satisfied then print statement
-}
-else {
-    console.log("pass");                           //if both condition failed 
-
-};
+// /*------if,if else,elseif------- */
 
 
-/* --------- switch statement ----------*/
 
-let price = 20;
+// let num = 18;                            //giving a value to num 
+// if (num <= 15) {                             //checking if condition
+//     console.log("Fail")                              //if condition satisfy print statement
+// }
+// else if (num > 15 && num < 20)                      //checking else if condition    
+// {
+//     console.log("passed with grace");           //if condition satisfied then print statement
+// }
+// else {
+//     console.log("pass");                           //if both condition failed 
 
-switch (price) {
-    case 30:
-        console.log("no")
-        break;
-
-        case 20:
-        console.log("think about it")
-         break;
-
-    default:
-        console.log("yes")
-        break;
-}
+// };
 
 
-/*----- for loop ------*/
+// /* --------- switch statement ----------*/
 
-let numb=1;
+// let price = 20;
 
-for(i=0;i<=10;i=i+1)
-{
-    console.log(numb);
-};
+// switch (price) {
+//     case 30:
+//         console.log("no")
+//         break;
+
+//         case 20:
+//         console.log("think about it")
+//          break;
+
+//     default:
+//         console.log("yes")
+//         break;
+// }
 
 
-/*---------------------for of-------------------------------- */
+// /*----- for loop ------*/
 
-let myName="Gagan"
-for(let char of myName){
-    console.log("my chars _"+char)
-};
+// let numb=1;
 
-/*-------------------------for in------------------------------------ */
+// for(i=0;i<=10;i=i+1)
+// {
+//     console.log(numb);
+// };
 
-const myObject={
-    a:1,
-    b:2,
-    c:3,
-}
-for(let key in myObject){
-    console.log("key are : " +key)
-}
 
-/*....................................................................... */
+// /*---------------------for of-------------------------------- */
 
-for(i=0;i<=7;i=i+1)
-    {
-        console.log (Math.floor(Math.random()*10)+1);
-    };
+// let myName="Gagan"
+// for(let char of myName){
+//     console.log("my chars _"+char)
+// };
 
-    /*------------while-----------*/
-    let v=1;
+// /*-------------------------for in------------------------------------ */
+
+// const myObject={
+//     a:1,
+//     b:2,
+//     c:3,
+// }
+// for(let key in myObject){
+//     console.log("key are : " +key)
+// }
+
+// /*....................................................................... */
+
+// for(i=0;i<=7;i=i+1)
+//     {
+//         console.log (Math.floor(Math.random()*10)+1);
+//     };
+
+//     /*------------while-----------*/
+//     let v=1;
     
-    while(v<10){
-        console.log("he");
+//     while(v<10){
+//         console.log("he");
 
-            ++v;
+//             ++v;
+//     }
+
+
+//     /*------------ Do while----------------- */
+// let t=0;
+//     do{
+//         console.log("hi bro");
+//         t++;
+//     } while(t<10);
+
+
+//     /*------------------objects---------------------*/
+
+//     const myobj={
+//         name:"Gagan",
+//         age:20,
+//         relation:"single",
+//         bday:"9/5/04"
+//     };
+//     console.log(myobj.relation);
+
+//     /*----------array------------------------------- */
+
+//     let randoms=[1,2,3,4,5,6,7,8,9,5,4,3];
+//     console.log(randoms.length);
+//     console.log(randoms.at(0));
+//     console.log(randoms.pop());
+//     console.log(randoms);
+//     console.log(randoms.push(4));
+//     console.log(randoms);
+//     console.log(randoms.shift());
+//     console.log(randoms);
+//     console.log(randoms.unshift(1));
+//     console.log(randoms);
+//     console.log(randoms.{copyWithin}(2,0));
+//     console.log(randoms);
+//     console.log(randoms.slice(1,3));
+//     console.log(randoms.splice(3,2));
+//     console.log(randoms.reverse());
+//     console.log(randoms.indexOf(7));
+
+
+
+//     // -------   for each method   ---------  //
+
+// let Arre = [23, 212, 9, 628, 22314];
+
+// function itrFunction(value,index) {
+// 	console.log(value,index);
+// }
+
+// Arre.forEach(itrFunction);
+
+
+// //-------------------map method------------------------//
+
+// let numArray = [1, 2, 3, 4];
+// let numArray2 = numArray.map(addFunction);
+
+// console.log(numArray2);
+
+// function addFunction(value) {
+// 	return value + 100;
+// }
+
+let arr=[1,2,5,6,9,11,45,3]
+let gn=0;
+for(i=0;i<=arr.length;i++){
+    if(gn<arr[i]){
+        (gn=arr[i]);  
+    }  
+}
+console.log(gn)
+
+//-------------- odd numbers -------------------
+
+console.log("Odd Number b/w 1 to n:")
+let n=6;
+for(let i=1;i<=n;i++){
+        if(i%2!=0){
+            console.log(i)
+        }
     }
 
 
-    /*------------ Do while----------------- */
-let t=0;
-    do{
-        console.log("hi bro");
-        t++;
-    } while(t<10);
+//--------------------even numbers------------------------
 
-
-    /*------------------objects---------------------*/
-
-    const myobj={
-        name:"Gagan",
-        age:20,
-        relation:"single",
-        bday:"9/5/04"
-    };
-    console.log(myobj.relation);
-
-    /*----------array------------------------------- */
-
-    let randoms=[1,2,3,4,5,6,7,8,9,5,4,3];
-    console.log(randoms.length);
-    console.log(randoms.at(0));
-    console.log(randoms.pop());
-    console.log(randoms);
-    console.log(randoms.push(4));
-    console.log(randoms);
-    console.log(randoms.shift());
-    console.log(randoms);
-    console.log(randoms.unshift(1));
-    console.log(randoms);
-    console.log(randoms.copyWithin(2,0));
-    console.log(randoms);
-    console.log(randoms.slice(1,3));
-    console.log(randoms.splice(3,2));
-    console.log(randoms.reverse());
-    console.log(randoms.indexOf(7));
-
-
-
-    // -------   for each method   ---------  //
-
-let Arre = [23, 212, 9, 628, 22314];
-
-function itrFunction(value,index) {
-	console.log(value,index);
-}
-
-Arre.forEach(itrFunction);
-
-
-//-------------------map method------------------------//
-
-let numArray = [1, 2, 3, 4];
-let numArray2 = numArray.map(addFunction);
-
-console.log(numArray2);
-
-function addFunction(value) {
-	return value + 100;
-}
-
+console.log("even Number b/w 1 to k:")
+let k=6;
+for(let i=1;i<=k;i++){
+        if(i%2==0){
+            console.log(i)
+        }
+    }
